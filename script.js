778ae5bee45028da62f1099d6c40da0d40e9ce6c
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-09-10 13:43:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
 // your code here
+	let a=0;
+	let b=1;
+	if (num === 0) return a;
+	if (num === 1) return b;
+	//iterate till the given num
+	for(let i=2; i<num; i++){
+		//sum of last two numbers 
+		let c=a+b;
+		//asign the last value to first
+		a=b;
+		//asign the sum to the last
+		b=c;
+	}
+	//if the num is 0 return a else return b
+	return b;
 }
 
 module.exports = fibonacci;
